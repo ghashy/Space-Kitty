a0d4fbb01ae45408d513bb72c91f7b7c0ae25b73
ghashy <obsidian.musicwork@gmail.com>
2023-06-11 18:57:53 +0300

    Update borders from camera projection system

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -117,6 +117,68 @@ pub fn spawn_world_borders(
     ));
 }
 
+pub fn update_borders(
+    mut commands: Commands,
+    mut window_resize_event: EventReader<WindowResized>,
+    mut walls_query: Query<(&mut Transform, Entity, &Wall)>,
+    camera_query: Query<
+        (&Transform, &OrthographicProjection),
+        (With<Camera2d>, Without<Wall>),
+    >,
+    mut local_timer: Local<(Timer, f32, f32)>,
+    mut false_on_start: Local<bool>,
+    time: Res<Time>,
+) {
+    if !*false_on_start {
+        *false_on_start = true;
+        local_timer.0.pause();
+    }
+
+    if let Some(event) = window_resize_event.iter().last() {
+        // Start timer
+        local_timer.1 = event.width;
+        local_timer.2 = event.height;
+        local_timer.0.set_mode(TimerMode::Once);
+        local_timer
+            .0
+            .set_duration(std::time::Duration::from_millis(1000));
+        local_timer.0.reset();
+        local_timer.0.tick(time.delta());
+        local_timer.0.unpause();
+    }
+
+    if !local_timer.0.finished() && !local_timer.0.paused() {
+        local_timer.0.tick(time.delta());
+
+        let horizontal = Collider::cuboid(local_timer.1, 2.);
+        let vertical = Collider::cuboid(2., local_timer.2);
+        let (cam_transform, cam_projection) = camera_query.single();
+        let (left, right, top, bottom) =
+            get_camera_borders(cam_transform, cam_projection.area);
+
+        for (mut wall_transform, entity, wall) in walls_query.iter_mut() {
+            match wall {
+                Wall::Top => {
+                    wall_transform.translation = top;
+                    commands.entity(entity).insert(horizontal.clone());
+                }
+                Wall::Bottom => {
+                    wall_transform.translation = bottom;
+                    commands.entity(entity).insert(horizontal.clone());
+                }
+                Wall::Left => {
+                    wall_transform.translation = left;
+                    commands.entity(entity).insert(vertical.clone());
+                }
+                Wall::Right => {
+                    wall_transform.translation = right;
+                    commands.entity(entity).insert(vertical.clone());
+                }
+            }
+        }
+    }
+}
+
 pub fn despawn_borders(
     mut commands: Commands,
     borders_query: Query<Entity, With<Wall>>,
